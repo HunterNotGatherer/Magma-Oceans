7a651bfc48f6c74cf76f433373937b0bfe99e81a
HunterNotGatherer <97756975+HunterNotGatherer@users.noreply.github.com>
2023-02-18 16:39:26 -0800

    Cleaning

--- Clean.py
+++
@@ -1,4 +0,0 @@
-import os
-for i in ["CoefGrid.txt","Model_sigma_Grid.txt","F_bar.dat","init_du.dat","rho_u_S9999.dat"]:
-    if os.path.exists(i): os.remove(i)
-x=[os.remove("coolData/"+i) for i in os.listdir("coolData/")]
