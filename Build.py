86315f8ab8cbc9349637feb3ca2ff88ad5d3812a
HunterNotGatherer <97756975+HunterNotGatherer@users.noreply.github.com>
2023-02-19 16:18:20 -0800

    Final Build?
    Plotting and statistics next.

--- Build.py
+++ Build.py
@@ -4,9 +4,11 @@ for i in Dep:
     with open(i,'w') as f:
         [print(line,end='',file=f) for line in open("Dependencies/"+i)]
     
-import BarF_Interp, Coef_Interp, Sigma_Interp, Rho_P_U_Interp, Init_Du
+import BarF_Interp, Coef_Interp, Sigma_Interp, Rho_P_U_Interp
+isBuilt=True
+for i in ["CoefGrid.txt","Model_sigma_Grid.txt","F_bar.dat","rho_u_S9999.dat"]:
+    if not os.path.exists(i): isBuilt=False
+if isBuilt: import Init_Du
 
 for i in Dep:
     if os.path.exists(i): os.remove(i)
-
-if not os.path.exists("coolData"): os.makedirs("coolData")
